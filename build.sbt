2c825ff2d3213a8cfee65930098d00933bff266c
noteisenheim <noteisenheim@gmail.com>
2020-10-04 17:59:41 +0300

    +class for tokenizing and clearing +configs for intellij

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "assignment2"
 version := "0.1"
 
 scalaVersion := "2.13.3"
+
+libraryDependencies ++= Seq( "org.apache.spark" % "spark-core_2.11" % "2.1.0")
+libraryDependencies ++= Seq( "org.apache.spark" % "spark-mllib_2.11" % "2.1.0")
+libraryDependencies ++= Seq( "org.apache.spark" % "spark-sql_2.11" % "2.1.0")
--- src/main/scala/Preprocessor.scala
+++ src/main/scala/Preprocessor.scala
@@ -1,15 +1,36 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd._
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover, Tokenizer}
 
 class Preprocessor {
-  case class Tweet(sentiment: Int, text: String)
-  val conf = new SparkConf().setAppName("Preprocessor")
-  val sc = new SparkContext(conf)
 
-  val train_data_file =  sc.textFile("test.csv")
+  def clear(file_path: String, sc: SparkContext) {
+    val sqlContext= new SQLContext(sc)
+    import sqlContext.implicits._
 
-  val train_data_x = train_data_file.map(line => (line.split(",")(1), line.split(",")(2)))
-                      .map(x => (x._1, x._2.split(" ").toSeq))
-                      .map(x => (x._1, x._2.filter(_.nonEmpty)))
-                      .collect()
+    val train_data_file = sc.textFile(file_path)
+
+    val train_data_unsliced = train_data_file.map(line => (line.split(",")(0), line.split(",")(1), line.split(",")(2))).collect()
+
+    val train_data = sc.parallelize(train_data_unsliced.slice(1, train_data_unsliced.length)).
+      map(x => (x._1.toInt, x._2.toInt, x._3)).
+      toDF("id", "sentiment", "sentence")
+
+    val regexTokenizer = new RegexTokenizer().
+      setInputCol("sentence").
+      setOutputCol("words").
+      setPattern("\\w+").setGaps(false)
+
+    val train_tokenized = regexTokenizer.transform(train_data)
+
+    val remover = new StopWordsRemover().
+      setInputCol("words").
+      setOutputCol("filtered")
+
+    val train_cleared = remover.transform(train_tokenized).select("id", "sentiment", "filtered")
+    train_cleared
+  }
+
+}
