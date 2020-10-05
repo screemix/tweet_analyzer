1d30d5722b75ea98ea56327a53d56365f069e6bd
noteisenheim <noteisenheim@gmail.com>
2020-10-05 19:20:03 +0300

    +preprocessing package

---
+++ src/main/scala/TweetAnalysis.scala
@@ -0,0 +1,28 @@
+import org.apache.spark
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd._
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
+import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover}
+import org.apache.spark.ml.feature.{HashingTF, IDF}
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.functions._
+import preprocessor.Preprocessor
+
+object TweetAnalysis {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("appName")
+    val sc = new SparkContext(conf)
+    val preprocessor = new Preprocessor()
+
+    val inputPath = "data/train.csv"
+    val outputPath = "test_preprocess_out.csv"
+    val tweet = "@_metallicar You mean @deanwinchester wouldn't like my beer flavoured bikini? Ah well. Have to shut your door, wings soaped up next"
+
+    val train_cleared = preprocessor.clear_train(inputPath, sc)
+    train_cleared.show(20)
+    val tweet_cleared = preprocessor.clear_input(tweet, sc)
+    tweet_cleared.show(false)
+  }
+}
---
+++ src/main/scala/preprocessor/Preprocessor.scala
@@ -0,0 +1,105 @@
+package preprocessor
+
+import org.apache.spark.SparkContext
+import org.apache.spark.ml.feature.{HashingTF, IDF, RegexTokenizer, StopWordsRemover}
+import org.apache.spark.sql.{DataFrame, SQLContext}
+
+class Preprocessor {
+
+  def parse_alias(row: (Int, Int, String)) : (Int, Int, String, Int) = {
+    // checks if there is an alias in tweet
+    // adds a new column to indicate that
+    val regexp = "@[a-zA-Z0-9_-]*".r
+    val aliases = regexp.findAllIn(row._3).toArray
+    if (aliases.length == 0) {
+      (row._1, row._2, row._3, 0)
+    }
+    else {
+      (row._1, row._2, regexp.replaceAllIn(row._3, ""), 1)
+    }
+  }
+
+  def parse_repetitions(row: (Int, Int, String, Int)) : (Int, Int, String, Int, Int) = {
+    // checks if there are repetitive characters (more than 2)
+    // if so replaces them with one
+    val regexp = "([a-z])\\1{2,}".r
+    val aliases = regexp.findAllIn(row._3).toArray
+    if (aliases.length == 0) {
+      (row._1, row._2, row._3, row._4, 0)
+    }
+    else {
+      (row._1, row._2, regexp.replaceAllIn(row._3, "$1"), row._4, 1)
+    }
+  }
+
+
+  def clear_seq(input: Seq[(Int, Int, String)], sc: SparkContext) : DataFrame = {
+
+    val sqlContext= new SQLContext(sc)
+    import sqlContext.implicits._
+
+    // remove aliases, links, html headers, repetitions
+    val cleared_df = input.
+      map(x => (x._1, x._2, "&(lt)?(gt)?(amp)?(quot)?;".r.replaceAllIn(x._3, ""))).
+      map(x => (x._1, x._2, "https?:[//.,!?a-zA-Z0-9]*".r.replaceAllIn(x._3, ""))).
+      map(x => parse_alias(x)).
+      map(x => parse_repetitions(x)).
+      map(x => if (x._3.contains('!')) (x._1, x._2, x._3, x._4, x._5, 1) else (x._1, x._2, x._3, x._4, x._5, 0)).
+      toDF("id", "sentiment", "text", "alias", "repetition", "exclamation")
+
+    // tokenize sentences
+    val regexTokenizer = new RegexTokenizer().
+      setInputCol("text").
+      setOutputCol("words").
+      setPattern("\\w+").setGaps(false)
+
+    val train_tokenized = regexTokenizer.transform(cleared_df)
+
+    // remove stop words
+    val remover = new StopWordsRemover().
+      setInputCol("words").
+      setOutputCol("filtered")
+
+    // return cleared dataframe
+    // columns: id, sentiment, filtered tokens, alias, repitiion, exclamation
+    val cleared = remover.transform(train_tokenized).select("id", "sentiment", "filtered", "alias", "repetition", "exclamation")
+    cleared
+  }
+
+
+  def clear_train(file_path: String, sc: SparkContext) : DataFrame = {
+
+    val sqlContext= new SQLContext(sc)
+    import sqlContext.implicits._
+
+    // read the train.csv
+    // columns: id, sentiment, tweet text
+
+    val spark = org.apache.spark.sql.SparkSession.builder.
+      master("local").
+      appName("CSV reader").
+      getOrCreate;
+
+    // read csv file
+    val train_data = spark.read.format("csv").
+      option("header", "true").
+      load(file_path).
+      map(x => (x.getAs[String](0).toInt, x.getAs[String](1).toInt, x.getAs[String](2))).
+      collect()
+
+    // perform preprocessing
+    val cleared_df = clear_seq(train_data, sc)
+    cleared_df
+  }
+
+  def clear_input(tweet: String, sc: SparkContext): DataFrame ={
+
+    // convert to sequence by adding rows
+    val s = Seq((1, 0, tweet))
+
+    // perform preprocessing
+    val cleared_tweet = clear_seq(s, sc)
+    cleared_tweet.select("id", "filtered", "alias", "repetition", "exclamation")
+  }
+
+}
