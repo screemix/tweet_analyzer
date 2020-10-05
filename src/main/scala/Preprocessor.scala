91e06342d8e5dc15c672b9e93538bd79bddab369
Alla <chepurova@bk.ru>
2020-10-05 23:50:03 +0300

    added word2vec, random forest

--- src/main/scala/Preprocessor.scala
+++ src/main/scala/Preprocessor.scala
@@ -4,6 +4,9 @@ import org.apache.spark.rdd._
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover, Tokenizer}
 import org.apache.spark.ml.feature.{HashingTF, IDF}
+import org.apache.spark.ml.feature.Word2Vec
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForestRegressor}
 
 class Preprocessor {
 
@@ -15,9 +18,8 @@ class Preprocessor {
 
     val train_data_unsliced = train_data_file.map(line => (line.split(",")(0), line.split(",")(1), line.split(",")(2))).collect()
 
-      val train_data =  (train_data_unsliced.slice(1, train_data_unsliced.length)).
-      map(x => (x._1.toInt, x._2.toInt, x._3)).
-      toDF("id", "sentiment", "sentence")
+    val train_data = sc.parallelize(train_data_unsliced.slice(1, train_data_unsliced.length))
+      .map(x => (x._1.toInt, x._2.toInt, x._3)).toDF("id", "sentiment", "sentence")
 
     val regexTokenizer = new RegexTokenizer().
       setInputCol("sentence").
@@ -34,7 +36,7 @@ class Preprocessor {
     train_cleared
   }
 
-  def tfIdf(data: sql.DataFrame ): Unit ={
+  def tfIdf(data: sql.DataFrame): Unit ={
     val zero_sent = data.filter("sentiment == 0")
     val  one_sent = data.filter("sentiment == 1")
 
@@ -50,7 +52,24 @@ class Preprocessor {
     val rescaled = rescaledOne.union(rescaledZero)
     rescaled
   }
+
+  def word2Vec(train_cleared: sql.DataFrame): Unit = {
+    val word2Vec = new Word2Vec().setInputCol("filtered").setOutputCol("vec").setVectorSize(3).setMinCount(0)
+    val model = word2Vec.fit(train_cleared)
+    val result = model.transform(train_cleared)
+    result
+  }
 }
+
+  def randForest(result: sql.DataFrame): Unit = {
+    val rf = new RandomForestRegressor().setLabelCol("sentiment").setFeaturesCol("vec")
+    val model = rf.fit(result)
+    val predictions = model.transform(result)
+    val preds = predictions.withColumn("rounded_prediction", round(col("prediction")))
+    val evaluator = new RegressionEvaluator().setLabelCol("sentiment").setPredictionCol("rounded_prediction").setMetricName("rmse")
+    val rmse = evaluator.evaluate(preds)
+    rmse
+  }
 
 class clearInput {
   def classify(twit: String): Unit ={
@@ -58,7 +77,7 @@ class clearInput {
     val tweet_df = Seq(processed_tweet).toDF("sentence")
     val regexTokenizer = new RegexTokenizer().setInputCol("sentence").setOutputCol("words").setPattern("\\w+").setGaps(false)
     val tweet_tokenized = regexTokenizer.transform(tweet_df)
-
+val result = model.transform(train_cleared)
     val remover = new StopWordsRemover().setInputCol("words").setOutputCol("filtered")
     val tweet_cleared = remover.transform(tweet_tokenized).select("sentence", "filtered")
 
