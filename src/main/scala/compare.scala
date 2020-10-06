356ecba248e24441cddb5cd0394ae4c2c148f706
Lev Svalov <leos3112@gmail.com>
2020-10-06 16:33:43 +0500

    add score to array and sort it. not completed

--- src/main/scala/compare.scala
+++ src/main/scala/compare.scala
@@ -1,8 +1,14 @@
-import org.apache.spark.{SparkConf, SparkContext}
+
+import org.apache.spark.{SPARK_BRANCH, SparkConf, SparkContext}
 import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
+import org.spark_project.dmg.pmml.True
+import scala.util.Sorting.quickSort
+import scala.math.Ordering.Double.compare
+import scala.math.Ordering.comparatorToOrdering
+import scala.util.Sorting.stableSort
 
 object compare {
   def main(args: Array[String]): Unit = {
@@ -10,8 +16,8 @@ object compare {
     conf.setMaster("local")
     conf.setAppName("compare models")
     val sc = new SparkContext(conf)
-//    val path = "/Users/levsvalov/code_workspace/Fall2020/IBD/spark/sample_classification_data.txt"
-    val path = args(0)
+    val path = "/Users/levsvalov/code_workspace/Fall2020/IBD/spark/sample_classification_data.txt"
+//    val path = args(0)
     val data = MLUtils.loadLibSVMFile(sc, path)
 
     val Array(training, test) = data.randomSplit(Array(0.6, 0.4), seed = 11L)
@@ -31,27 +37,50 @@ object compare {
 
     val metrics = new BinaryClassificationMetrics(predictionAndLabels)
 
-
     val f1Score = metrics.fMeasureByThreshold
+    val n = f1Score.count().toInt
+    var f1arr = new Array[(Double,Double)](n)
+    var i = 0
     f1Score.foreach { case (t, f) =>
-      println(s"Threshold: $t, F-score: $f, Beta = 1")
+      println(s"Threshold: $t, F-score: $f")
+
+      f1arr(i) = (t,f)
+      println(f1arr(i))
+      i = i + 1
     }
 
-    val beta = 0.5
-    val fScore = metrics.fMeasureByThreshold(beta)
-    fScore.foreach { case (t, f) =>
-      println(s"Threshold: $t, F-score: $f, Beta = 0.5")
+    println("array size:")
+    println(f1arr.size)
+    println("array:")
+    for ( (t,f) <-f1arr )
+    {
+      println(s"Threshold: ${t}, F-score: ${f}")
     }
 
-    val precision = metrics.precisionByThreshold
-    precision.foreach { case (t, p) =>
-      println(s"Threshold: $t, Precision: $p")
+    quickSort(f1arr)(Ordering.by[(Double, Double), Double](_._2))
+
+
+    println("Now sorted:")
+    for ( t <-f1arr )
+    {
+      println(s"Threshold: ${t._1}, F-score: ${t._2}")
     }
 
-    val recall = metrics.recallByThreshold
-    recall.foreach { case (t, r) =>
-      println(s"Threshold: $t, Recall: $r")
-    }
+//    val beta = 0.5
+//    val fScore = metrics.fMeasureByThreshold(beta)
+//    fScore.foreach { case (t, f) =>
+//      println(s"Threshold: $t, F-score: $f, Beta = 0.5")
+//    }
+//
+//    val precision = metrics.precisionByThreshold
+//    precision.foreach { case (t, p) =>
+//      println(s"Threshold: $t, Precision: $p")
+//    }
+//
+//    val recall = metrics.recallByThreshold
+//    recall.foreach { case (t, r) =>
+//      println(s"Threshold: $t, Recall: $r")
+//    }
 
   }
 }
