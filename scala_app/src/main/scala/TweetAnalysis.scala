0813cd77a7a44954951b2679616e3eb5ee84dad9
noteisenheim <noteisenheim@gmail.com>
2020-10-07 17:53:46 +0300

    stream processing, lr and rf models

---
+++ scala_app/src/main/scala/TweetAnalysis.scala
@@ -0,0 +1,170 @@
+import org.apache.spark
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd._
+import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
+import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover}
+import org.apache.spark.ml.feature.{HashingTF, IDF}
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.tuning.CrossValidatorModel
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.feature.Word2VecModel
+import java.io.File
+
+import preprocessor.Preprocessor
+import models.Models
+
+object TweetAnalysis {
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+
+    val inputPath = args(0)
+
+    val spark = SparkSession.builder().appName("very streaming very scala \uD80C\uDD8F").getOrCreate()
+    val sc = spark.sparkContext
+    val preprocessor = new Preprocessor()
+    val models = new Models()
+
+    // evaluate on train data
+    val trainedModels = evalOnTrainTest(inputPath, preprocessor, models, sc)
+    val w2vModel = trainedModels._1
+    val lrModel = trainedModels._2
+    val rfModel = trainedModels._3
+
+    // stream analysis
+    println("START STREAM PROCESSING")
+    val tweets = spark.readStream.format("socket").option("host", "10.90.138.32").option("port", "8989").load()
+
+    val notNull = tweets.filter("value != ''")
+
+    // val Models_predict = notNull.withColumn("1st model", AllaModel.predict(col("value").toString()))
+    // 							.withColumn("2nd model", AminaModel.predict(col("value").toString()))
+
+    val Models_predict = notNull.select(col("value"),
+      lit(predict(col("value").toString(), preprocessor, models, w2vModel, lrModel, sc)).as("1st model"),
+      lit(predict(col("value").toString(), preprocessor, models, w2vModel, rfModel, sc)).as("2nd model"))
+
+
+    val query = Models_predict.writeStream
+      .format("csv")
+      .option("format", "append")
+      .option("path", "hdfs://namenode:9000/user/sharpei/output/")
+      .option("checkpointLocation", "hdfs://namenode:9000/user/sharpei/checkpoint_dir/")
+      .outputMode("append")
+      .start()
+      .awaitTermination(60000 * 30)
+
+
+    spark.read.csv("hdfs://namenode:9000/user/sharpei/output/").coalesce(1).write.csv("hdfs://namenode:9000/user/sharpei/final_output/")
+
+  }
+
+
+  def predict(tweet: String, preprocessor: Preprocessor, models: Models, w2vModel: Word2VecModel, model: CrossValidatorModel, sc: SparkContext): Int = {
+    val tweetCleared = preprocessor.clear_input(tweet, sc)
+    val prediction = models.predict(tweetCleared, w2vModel, model, sc)
+    prediction
+  }
+
+
+  def evalOnTrainTest(inputPath: String, preprocessor: Preprocessor, models: Models, sc: SparkContext): (Word2VecModel, CrossValidatorModel, CrossValidatorModel) = {
+
+    // creates or loads models
+    //
+
+    val conf = sc.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+
+    // clear train file
+    println("start preprocessing")
+    val train_cleared = preprocessor.clear_train(inputPath, sc)
+
+    // train-test split
+    val Array(train, test) = train_cleared.randomSplit(Array[Double](0.7, 0.3))
+
+    // ----------------W2V MODEL----------------
+    var trainW2V = true
+    if (fs.exists(new org.apache.hadoop.fs.Path("w2vModel"))) {
+      println("loading existing word 2 vec model")
+      trainW2V = false
+    }
+
+    if (trainW2V) {
+      // train w2v model
+      println("start training word2vec")
+      val w2v_t = preprocessor.word2vec_train(train, 30, 10, "filtered", "features")
+      w2v_t.save("w2vModel")
+    }
+
+    // load model from file
+    val w2vModel = Word2VecModel.load("w2vModel")
+    // ----------------W2V MODEL----------------
+
+
+
+    // ----------------LOGREG MODEL----------------
+    println("---------------LOGISTIC REGRESSION---------------")
+
+    var trainLogreg = true
+    if (fs.exists(new org.apache.hadoop.fs.Path("logregModel"))) {
+      println("loading existing logistic regression model")
+      trainLogreg = false
+    }
+
+    if (trainLogreg) {
+      // train the model
+      println("start training logreg")
+      models.logregTrain(train, w2vModel, sc)
+    }
+
+    // load model from file
+    val lrModel = CrossValidatorModel.load("logregModel")
+
+    // evaluate on test data
+    val scores_lr = models.testEval(test, w2vModel, lrModel, sc)
+
+    println("SCORES ON TEST DATA")
+    print("precision: ")
+    println(scores_lr._1)
+    print("recall: ")
+    println(scores_lr._2)
+    print("f1 score: ")
+    println(scores_lr._3)
+    // ----------------LOGREG MODEL----------------
+
+
+
+    // ----------------RAND FOREST MODEL----------------
+    println("---------------RANDOM FOREST---------------")
+
+    var trainRF = true
+    if (fs.exists(new org.apache.hadoop.fs.Path("rfModel"))) {
+      println("loading existing random forest model")
+      trainRF = false
+    }
+
+    if (trainRF) {
+      // train the model
+      println("start training random forest")
+      models.rfTrain(train, w2vModel, sc)
+    }
+    // load model from file
+    val rfModel = CrossValidatorModel.load("rfModel")
+
+    // evaluate on test data
+    val scores_rf = models.testEval(test, w2vModel, rfModel, sc)
+
+    println("SCORES ON TEST DATA")
+    print("precision: ")
+    println(scores_rf._1)
+    print("recall: ")
+    println(scores_rf._2)
+    print("f1 score: ")
+    println(scores_rf._3)
+    // ----------------RAND FOREST MODEL----------------
+
+    (w2vModel, lrModel, rfModel)
+  }
+}
---
+++ scala_app/src/main/scala/models/Models.scala
@@ -0,0 +1,200 @@
+package models
+
+import org.apache.spark.SparkContext
+import org.apache.spark.api.java.JavaRDD.fromRDD
+import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover, Word2Vec, Word2VecModel}
+import org.apache.spark.sql.{DataFrame, Dataset, SQLContext}
+import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel, RandomForestClassifier}
+import org.apache.spark.ml.classification.RandomForestClassificationModel
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.ml.tuning.{CrossValidator, CrossValidatorModel, ParamGridBuilder}
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.rdd.RDD
+import preprocessor.Preprocessor
+
+class Models {
+
+
+  def evaluate_custom(predictionAndLabels: RDD[(Double, Double)]) : (Double, Double, Double) = {
+    var tp = 0
+    var tn = 0
+    var fp = 0
+    var fn = 0
+    for (data<-predictionAndLabels.collect()){
+      //println("iterate")
+      val prediction = data._1
+      val label = data._2
+      if (prediction==label){
+        if(prediction==1.0){
+          tp = tp + 1
+        }else{
+          tn = tn + 1
+        }
+      }else{
+        if(prediction==1.0){
+          fp = fp + 1
+        }else{
+          fn = fn + 1
+        }
+      }
+    }
+    val precision = tp.toDouble/(tp+fp).toDouble
+    val recall =  tp.toDouble/(tp+fn).toDouble
+    val f1Score = 2 * ((precision * recall) / (precision + recall))
+
+    (precision, recall, f1Score)
+  }
+
+
+  def evaluate(predictionAndLabels: RDD[(Double, Double)]): (Double, Double) = {
+
+    // calculate f1 score for each threshold
+    // output threshold and max f1 score
+    // precision for threshold is 0.01
+
+    val metrics = new BinaryClassificationMetrics(predictionAndLabels)
+
+    // auc score
+    val auPRC = metrics.areaUnderPR
+    println("Area under precision-recall curve = " + auPRC)
+
+    val f1Score = metrics.fMeasureByThreshold.
+      map(x => ((math rint x._1 * 100) / 100, x._2)).
+      filter(x => x._1 > 0.0).
+      reduceByKey((x, y) => (x + y) / 2.0).
+      collect()
+
+    val maxScore = f1Score.maxBy(_._2)
+    maxScore
+  }
+
+
+  def logregTrain(train: DataFrame, w2vModel: Word2VecModel, sc: SparkContext): Unit = {
+
+    // hyperparam tuning and training logistic regression
+    // model saved to logregModel
+
+    val sqlContext= new SQLContext(sc)
+    import sqlContext.implicits._
+
+    // vectorize train data
+    val trainVecLr = w2vModel.transform(train)
+
+    // transform train data to labeled point - UNNEEDED BUT WHATEVER
+    //val train_labeled = train_vec_lr.map(x => LabeledPoint(x.getAs[Int]("label").toDouble, Vectors.fromML(x.getAs[Vector]("features")))).rdd
+
+    // new logreg model, train
+    val lrModel = new LogisticRegression().
+      setFeaturesCol("features").
+      setLabelCol("label")
+
+    // ------------HYPERPARAM TUNING------------
+    // COMMENTED CODE USED FOR TUNING
+    // hyperparam tuning and crying
+    val paramGridLr = new ParamGridBuilder().
+      addGrid(lrModel.regParam, Array(0, 0.01, 0.1)).
+      addGrid(lrModel.threshold, Array(0.3, 0.4, 0.5, 0.6)).
+      addGrid(lrModel.fitIntercept, Array(true, false)).
+      build()
+
+    val crossvalLr = new CrossValidator()
+      .setEstimator(lrModel)
+      .setEvaluator(new BinaryClassificationEvaluator)
+      .setEstimatorParamMaps(paramGridLr)
+      .setNumFolds(5)
+
+    val cvModel = crossvalLr.fit(trainVecLr)
+
+    println("Logistic Regression - Best Params")
+    println(cvModel.bestModel.extractParamMap())
+
+    cvModel.save("logregModel")
+    // ------------HYPERPARAM TUNING------------
+  }
+
+
+  def rfTrain(train: DataFrame, w2vModel: Word2VecModel, sc: SparkContext): Unit = {
+
+    // hyperparam tuning and training logistic regression
+    // model saved to rfModel
+
+    val sqlContext= new SQLContext(sc)
+    import sqlContext.implicits._
+
+    // vectorize train data
+    val trainVecRf = w2vModel.transform(train)
+
+    // new random forest model, train
+    val rf = new RandomForestClassifier().
+      setLabelCol("label").
+      setFeaturesCol("features")
+
+    // ------------HYPERPARAM TUNING------------
+    // COMMENTED CODE USED FOR TUNING
+    // hyperparam tuning and crying
+    val paramGridRf = new ParamGridBuilder().
+      addGrid(rf.maxDepth, Array(5, 10)).
+      addGrid(rf.numTrees, Array(5, 10, 20, 30)).
+      build()
+
+    val crossvalRf = new CrossValidator()
+      .setEstimator(rf)
+      .setEvaluator(new BinaryClassificationEvaluator)
+      .setEstimatorParamMaps(paramGridRf)
+      .setNumFolds(5)
+
+    val cvModel = crossvalRf.fit(trainVecRf)
+
+    println("Random Forest - Best Params")
+    println(cvModel.bestModel.extractParamMap())
+
+    cvModel.save("rfModel")
+    // ------------HYPERPARAM TUNING------------
+  }
+
+  def testEval(test: DataFrame, w2vModel: Word2VecModel, model: CrossValidatorModel, sc: SparkContext): (Double, Double, Double) = {
+
+    // evaluate model on test dataset
+    // return precision, recall, f1 score
+
+    val sqlContext= new SQLContext(sc)
+    import sqlContext.implicits._
+
+    // vectorize test data
+    val testVec = w2vModel.transform(test)
+    // transform test data to labeledpoint
+    //val test_labeled = test_vec_lr.map(x => LabeledPoint(x.getAs[Int]("sentiment").toDouble, Vectors.fromML(x.getAs[Vector]("vec")))).rdd
+
+    val testTransf = model.transform(testVec)
+    // make predictions on test data
+    val predictionAndLabels = testTransf.map(x => (x.getAs[Vector]("probability")(1), x.getAs[Int]("label").toDouble)).rdd
+
+    // evaluate mode, find the best threshold
+    val maxScore = evaluate(predictionAndLabels)
+    val threshold = maxScore._1
+
+    // return (precision, recall, f1 score)
+    evaluate_custom(predictionAndLabels.map(x => if (x._1 > threshold) (1.0, x._2) else (0.0, x._2)))
+
+  }
+
+
+  def predict(tweet: DataFrame, w2vModel: Word2VecModel, model: CrossValidatorModel, sc: SparkContext): Int = {
+
+    // makes a prediction for a single tweet
+
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
+
+    // vectorize tweet
+    val tweetVec = w2vModel.transform(tweet)
+
+    // make prediction
+    val tweetTransf = model.transform(tweetVec)
+    tweetTransf.first().getAs[Double]("prediction").toInt
+  }
+
+}
